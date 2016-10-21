ce359ab651e31199053a69ddf383ef6c71318c11
extwiii <bcagiran@hotmail.com>
2016-10-21 21:02:11 +0100

    firing up basic passport app

---
+++ server/_config.js
@@ -0,0 +1,19 @@
+var ids = {
+  github: {
+    clientID: '8eb643543bfd712a6f4f',
+    clientSecret: 'eeb07dc7f99e944ea133323525c54b02b3b5af8c',
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  linkedin: {
+    clientID: '77lg2dpm0dpzlz',
+    clientSecret: 'eX9ePIkkXkuPQNnf',
+    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+  },
+  twitter: {
+    consumerKey: 'r2C46kHgdcJ21MsTV0MwfEcip',
+    consumerSecret: 'AoB6c4MMj8nhaUhr8zQIOLfpp0DLCy0hw5Lky0a2Y34o6ADjFg',
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  }
+};
+
+module.exports = ids;
