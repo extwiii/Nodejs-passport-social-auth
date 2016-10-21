357d69f7b097910a1490979dfc9f05fc10cb5817
Bilal CAGIRAN <bcagiran@hotmail.com>
2016-10-21 21:14:36 +0100

    change

--- server/_config.js
+++ server/_config.js
@@ -1,17 +1,17 @@
 var ids = {
   github: {
-    clientID: '8eb643543bfd712a6f4f',
-    clientSecret: 'eeb07dc7f99e944ea133323525c54b02b3b5af8c',
+    clientID: '<your-id>',
+    clientSecret: '<your-secret>',
     callbackURL: "http://127.0.0.1:3000/auth/github/callback"
   },
   linkedin: {
-    clientID: '77lg2dpm0dpzlz',
-    clientSecret: 'eX9ePIkkXkuPQNnf',
+    clientID: '<your-id>',
+    clientSecret: '<your-secret>',
     callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
   },
   twitter: {
-    consumerKey: 'r2C46kHgdcJ21MsTV0MwfEcip',
-    consumerSecret: 'AoB6c4MMj8nhaUhr8zQIOLfpp0DLCy0hw5Lky0a2Y34o6ADjFg',
+    consumerKey: '<your-key>',
+    consumerSecret: '<your-secret>',
     callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
   }
 };
